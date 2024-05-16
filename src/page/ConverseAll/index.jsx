545b6ae0461d64c953a33aa5e0722f8bfbc9a05b
QuangAnhTo <quangato072@gmail.com>
2024-05-16 15:15:41 +0700

    feat: khởi tạo trang about và converse all

---
+++ src/page/ConverseAll/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function ConverseAll() {
+  return (
+    <>
+      <Link to="/">Home</Link>
+      <h1>Converse All</h1>
+      <p>This is the Converse All page</p>
+    </>
+  );
+}
---
+++ src/page/about/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function About() {
+  return (
+    <>
+        <Link to="/">Home</Link>
+        <h1>About</h1>
+        <p>This is the about page</p>
+    </>
+  )
+}
