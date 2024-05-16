bb5fa751232f0d87625459d29117ec5497d98248
QuangAnhTo <quangato072@gmail.com>
2024-05-16 16:02:13 +0700

    chore: Add Layout component for consistent page structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import "./App.css";
 import router from "./react-routing/routes";
 import Navbar from "./components/navigations/navbar";
 import { RouterProvider } from "react-router-dom";
+import Layout from "./components/navigations/Layout";
 
 function App() {
     console.log(router);
   return (
     <RouterProvider router={router}>
       <div>
-        <Navbar />
+        <Layout />
         <div id="root"></div>
       </div>
     </RouterProvider>
--- src/react-routing/routes.jsx
+++ src/react-routing/routes.jsx
@@ -1,22 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Link } from "react-router-dom";
 import About from "../page/about";
-import Navbar from "../components/navigations/navbar";
 import ConverseAll from "../page/ConverseAll";
+import Layout from "../components/navigations/Layout";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Navbar />
-    },
-    {
-        path: "/about",
-        element: <About />
-    },
-    {
-        path: "/converseall",
-        element: <ConverseAll />
-    }
+  {
+    path: "/",
+    element: <Layout />, // Wrap routes with Layout for consistent structure
+    children: [
+      // Home route nested within Layout
+      { path: "about", element: <About /> }, // About route nested within Layout
+      { path: "converseall", element: <ConverseAll /> }, // ConverseAll route nested within Layout
+    ],
+  },
 ]);
 
 export default router;
