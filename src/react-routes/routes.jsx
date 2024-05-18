ca5cfe9cc5056382e755b77100d0f601c2bade6c
QuangAnhTo <quangato072@gmail.com>
2024-05-18 12:19:46 +0700

    feat: Add Notfound and Product_detail pages

--- src/react-routes/routes.jsx
+++ src/react-routes/routes.jsx
@@ -3,6 +3,8 @@ import About from "../page/About";
 import ConverseAll from "../page/ConverseAll";
 import Layout from "../components/navigations/Layout";
 import Homepage from "../page/Homepage";
+import Product_detail from "../page/Product_detail";
+import Notfound from "../page/Notfound";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +15,8 @@ const router = createBrowserRouter([
       { path: "/", element: <Homepage /> },
       { path: "about", element: <About /> }, // About route nested within Layout
       { path: "converseall", element: <ConverseAll /> }, // ConverseAll route nested within Layout
+      { path: "converseall/product/:id", element: <Product_detail /> }, // Product Detail route nested within Layout
+      { path: "*", element: <Notfound />}
     ],
   },
 ]);
