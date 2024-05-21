6166fde591339c0078a1e067269681bd3d32497b
QuangAnhTo <quangato072@gmail.com>
2024-05-21 10:45:23 +0700

    chore: Update navigation bar component and styles

--- src/components/navigations/Navbar.jsx
+++ src/components/navigations/Navbar.jsx
@@ -16,18 +16,30 @@ export default function Navbar({ className }) {
             alt="logo"
           />
         </NavLink>
-        <NavLink to="/converseall">ConverseAll</NavLink>
+        <NavLink to="/converseall">All products</NavLink>
         <NavLink to="/about">About</NavLink>
       </div>
       <div className="flex flex-row w-100 gap-10 font-sans font-bold">
         <input
           className="w-52 rounded-md px-5 py-2 border-2 border-gray-300 focus:outline-none focus:border-gray-500"
           type="text"
-          placeholder= "Search"
+          placeholder="Search"
         />
-        <NavLink className="text-2xl" to="/login"><IoLogInOutline/></NavLink>
-        <NavLink className="text-2xl" to="/favourites"><IoHeartOutline/></NavLink>
-        <NavLink className="text-2xl" to="/cart"><IoCartOutline/></NavLink>
+        <NavLink
+          className="text-2xl"
+          to="/login">
+          <IoLogInOutline />
+        </NavLink>
+        <NavLink
+          className="text-2xl"
+          to="/favourites">
+          <IoHeartOutline />
+        </NavLink>
+        <NavLink
+          className="text-2xl"
+          to="/cart">
+          <IoCartOutline />
+        </NavLink>
       </div>
     </nav>
   );
