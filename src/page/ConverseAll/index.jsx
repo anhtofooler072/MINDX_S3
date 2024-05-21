068d1a87b2544d450d26ae3563211646598aad6e
QuangAnhTo <quangato072@gmail.com>
2024-05-21 10:46:51 +0700

    feat: Add lazy loading for images in ConverseAll page and update Product_detail page layout

--- src/page/ConverseAll/index.jsx
+++ src/page/ConverseAll/index.jsx
@@ -1,10 +1,30 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 export default function ConverseAll() {
+  const navigate = useNavigate();
+  const [products, setProducts] = useState([]); // [1]
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await axios.get("https://api-project-testing-mndx3.onrender.com/products"); // [2]
+      setProducts(response.data);
+    };
+    fetchProducts();
+  }, []);
+
+  console.log(products); // [3]
+
   return (
     <>
       <h1 className="italic text-green-600">Converse All</h1>
       <p>This is the Converse All page</p>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => navigate("/converseall/product/664b16e6348b3f0b607836f4")}
+      >
+        Go to Product Detail
+      </button>
     </>
   );
 }
--- src/page/Product_detail/index.jsx
+++ src/page/Product_detail/index.jsx
@@ -4,9 +4,15 @@ import { useParams } from "react-router-dom";
 export default function Product_detail() {
   const { id } = useParams();
   return (
-    <div>
-      <h1>Product Detail</h1>
-      <p>Product ID: {id}</p>
+    <div className="h-screen">
+      <div className="flex px-14 py-7 h-96 w-full justify-center items-center bg-cover bg-no-repeat object-cover" style={{backgroundImage : `url(https://bizweb.dktcdn.net/100/493/370/themes/940719/assets/main_product_breadcrumb_bg.jpg?1713464283843)`}}>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus consequatur in itaque assumenda nam consequuntur dolorem soluta dolor doloremque voluptas facere explicabo neque praesentium, non quae impedit officia ea quos?</p>
+      </div>
+      <div>
+        <h1>Product Detail</h1>
+        <p>Product ID: {id}</p>
+        <p>lorem</p>
+      </div>
     </div>
   );
 }
