bec8d7efc594d9e54ce08aa4fd595fe6c4c9057a
QuangAnhTo <quangato072@gmail.com>
2024-05-22 16:10:44 +0700

    chore: Add Redux store and slice for managing products

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import "./App.css";
 import router from "./react-routes/routes";
 import { RouterProvider } from "react-router-dom";
 import Layout from "./components/navigations/Layout";
+import {Provider} from "react-redux";
+import store from "./store/store"; 
+
 
 function App() {
   // console.log(router);
   return (
+    <Provider store={store}>
     <RouterProvider router={router}>
       <div>
         <Layout />
         <div id="root"></div>
       </div>
     </RouterProvider>
+    </Provider>
   );
 }
 
