ced818385e062e1a9b65928afb3484fb8c4a6e75
QuangAnhTo <quangato072@gmail.com>
2024-05-22 15:57:38 +0700

    feat: Update ConverseAll page to use Redux for fetching products

--- src/page/ConverseAll/index.jsx
+++ src/page/ConverseAll/index.jsx
@@ -1,30 +1,55 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import { fetchProducts } from "../../store/slice";
+import { useDispatch, useSelector } from "react-redux";
+import ReactLoading from "react-loading";
+
 export default function ConverseAll() {
   const navigate = useNavigate();
-  const [products, setProducts] = useState([]); // [1]
+  // const [products, setProducts] = useState([]); // [1]
+
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.products.products);
+  const status = useSelector((state) => state.products.status);
+  const error = useSelector((state) => state.products.error);
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const response = await axios.get("https://api-project-testing-mndx3.onrender.com/products"); // [2]
-      setProducts(response.data);
-    };
-    fetchProducts();
-  }, []);
+    dispatch(fetchProducts());
+  }, [dispatch]);
 
-  console.log(products); // [3]
+  while (status === "loading") {
+    return (
+      <ReactLoading
+        type={"spin"}
+        color={"2f383e"}
+        height={200}
+        width={200}
+      />
+    );
+  }
 
-  return (
-    <>
-      <h1 className="italic text-green-600">Converse All</h1>
-      <p>This is the Converse All page</p>
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={() => navigate("/converseall/product/664b16e6348b3f0b607836f4")}
-      >
-        Go to Product Detail
-      </button>
-    </>
-  );
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (status === "succeeded") {
+    return (
+      <div>
+        {products.map((product) => (
+          <div
+            key={product._id}
+            onClick={() => {
+              navigate(`/converseall/product/${product._id}`);
+            }}>
+            <h1>{product.name}</h1>
+            <img
+              src={product.images.img_3}
+              alt={product.name}
+            />
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
