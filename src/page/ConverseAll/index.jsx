06af811cbe2fd891f295e6132c946ca249800b1b
QuangAnhTo <quangato072@gmail.com>
2024-05-22 16:24:09 +0700

    add reactloading

--- src/page/ConverseAll/index.jsx
+++ src/page/ConverseAll/index.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+// import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../../store/slice";
+import ReactLoading from "react-loading";
 
 export default function ConverseAll() {
   const navigate = useNavigate();
@@ -17,7 +18,7 @@ export default function ConverseAll() {
   }, [dispatch]);
 
   while (status === "loading") {
-    return <div>Loading...</div>;
+    return <ReactLoading type={"spin"} color={"2f383e"} height={50} width={50} />;
   }
 
   if (status === "failed") {
