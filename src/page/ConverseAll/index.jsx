0d974d15acd2471655744e7b7253cd4a2c0f7a4a
QuangAnhTo <quangato072@gmail.com>
2024-05-22 22:08:57 +0700

    styling grid

--- src/page/ConverseAll/index.jsx
+++ src/page/ConverseAll/index.jsx
@@ -1,13 +1,11 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-// import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../../store/slice";
 import ReactLoading from "react-loading";
 
 export default function ConverseAll() {
   const navigate = useNavigate();
-  // const [products, setProducts] = useState([]); // [1]
 
   const dispatch = useDispatch();
   const products = useSelector((state) => state.products.products);
@@ -21,9 +19,8 @@ export default function ConverseAll() {
     return (
       <ReactLoading
         type={"spin"}
-        color={"2f383e"}
-        height={50}
-        width={50}
+        color={"#fc531b"}
+        className="mx-auto mt-10"
       />
     );
   }
@@ -35,19 +32,26 @@ export default function ConverseAll() {
   if (status === "success") {
     return (
       <div>
-        <h1>Converse All</h1>
-        <div className="converse-all">
+        <h1 className="text-xl font-extrabold text-center mt-20">
+          Converse All
+        </h1>
+        <div className="grid grid-cols-4 gap-8 mx-20 my-10 place-items-center">
           {products.map((product) => (
             <div
               key={product._id}
-              className="product"
+              className="flex flex-col items-center justify-center w-60 h-auto py-10 px-5 border border-gray-300 rounded-lg shadow-md cursor-pointer hover:shadow-lg transition duration-300"
               onClick={() => navigate(`/converseall/product/${product._id}`)}>
               <img
                 src={product.images.img_2}
                 alt={product.name}
+                className="w-32 h-32 mb-10"
               />
-              <p>{product.name}</p>
-              <p>{product.price}</p>
+              <span className="text-gray-800 font-light mb-5 text-center">
+                {product.name}
+              </span>
+              <span className="font-semibold">
+                {Number(product.cost).toLocaleString()} đ
+              </span>
             </div>
           ))}
         </div>
