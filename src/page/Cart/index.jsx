7d2d31dcda4d215dbeddf076aeae0c949690f131
QuangAnhTo <quangato072@gmail.com>
2024-06-01 22:13:29 +0700

    Refactor: optimized for rendering list

--- src/page/Cart/index.jsx
+++ src/page/Cart/index.jsx
@@ -56,84 +56,81 @@ export default function Cart() {
   console.log(cart);
   if (cart === null || cart.length === 0) {
     return (
-      <div className="flex flex-col items-center justify-around gap-7 mt-60">
+      <div className="mb-80 mt-60 flex flex-col items-center justify-around gap-7">
         <h1 className="mt-8 text-5xl font-light text-gray-300">
           Your Cart Is Empty
         </h1>
-          <TbShoppingCartOff className="w-40 h-40 text-gray-300" />
+        <TbShoppingCartOff className="h-40 w-40 text-gray-300" />
       </div>
     );
   }
 
-  if (status === "failed") {
-    return <div>Failed to fetch data</div>;
-  } else if (status === "success") {
-    return (
-      <>
-        <h1 className="mb-10 mt-8 text-center text-4xl font-bold">Your Cart</h1>
-        <div className="mx-36 flex h-auto w-auto flex-col items-end">
-          <button
-            className="flex w-40 flex-row items-center justify-between divide-x-2 divide-white rounded bg-red-500 px-4 py-2 font-bold text-white transition duration-200 hover:bg-orange-700 hover:shadow-md"
-            onClick={handleDeleteAll}
-          >
-            <span className="text-sm">Delete All</span>
-            <RiDeleteBin6Line className="w-1/3 pl-4 text-white" />
-          </button>
-          <div className="mt-8 flex h-auto w-full flex-col items-center justify-center divide-y-2 rounded-lg border border-gray-300 p-5 shadow-md">
-            {Array.isArray(products) &&
-              products.map((product) => {
-                const item = cart.find((item) => item.id === product._id);
-                if (item) {
-                  return (
+  return (
+    <>
+      <h1 className="mb-10 mt-8 text-center text-4xl font-bold">Your Cart</h1>
+      <div className="mx-36 mb-32 flex h-auto w-auto flex-col items-end">
+        <button
+          className="flex w-40 flex-row items-center justify-between divide-x-2 divide-white rounded bg-red-500 px-4 py-2 font-bold text-white transition duration-200 hover:bg-orange-700 hover:shadow-md"
+          onClick={handleDeleteAll}
+        >
+          <span className="text-sm">Delete All</span>
+          <RiDeleteBin6Line className="w-1/3 pl-4 text-white" />
+        </button>
+        <div className="mt-8 flex h-auto w-full flex-col items-center justify-center divide-y-2 rounded-lg border border-gray-300 p-5 shadow-md">
+          {Array.isArray(products) &&
+            products.map((product) => {
+              const item = cart.find((item) => item.id === product._id);
+              if (item) {
+                return (
+                  <div
+                    key={product._id}
+                    className="flex h-auto w-full cursor-pointer flex-row justify-between justify-items-center gap-5 px-3 py-5 transition duration-100 hover:bg-gray-100 hover:shadow-lg"
+                  >
+                    <img
+                      src={product.images.img_2}
+                      alt={product.name}
+                      className="h-32 w-32 rounded-full border border-gray-300 object-cover object-center shadow-md transition duration-300 hover:border-orange-500"
+                    />
                     <div
-                      key={product._id}
-                      className="flex h-auto w-full cursor-pointer flex-row justify-between justify-items-center gap-5 rounded-md px-3 py-5 transition duration-300 hover:border-orange-500 hover:shadow-md"
+                      className="flex h-auto w-3/4 flex-row items-center justify-end gap-7"
+                      onClick={() =>
+                        navigate(`/converseall/product/${product._id}`)
+                      }
                     >
-                      <img
-                        src={product.images.img_2}
-                        alt={product.name}
-                        className="h-32 w-32 rounded-full border border-gray-300 object-cover object-center shadow-md transition duration-300 hover:border-orange-500"
-                        onClick={() =>
-                          navigate(`/converseall/product/${product._id}`)
-                        }
-                      />
-                      <div className="flex flex-row gap-7">
-                        <p className="translate-y-1/2 font-bold">
-                          {product.name}
-                        </p>
-                        <p className="translate-y-1/2">
-                          giá: {Number(product.cost).toLocaleString()} đ
-                        </p>
-                        <p className="translate-y-1/2">
-                          Quantity: {item.quantity}
-                        </p>
-                        <button
-                          onClick={() => {
-                            deleteIndividual(product._id);
-                            setTotalPrice(
-                              totaPrice - product.cost * item.quantity,
-                            );
-                          }}
-                        >
-                          <IoIosCloseCircle className="text-xl text-red-500" />
-                        </button>
-                      </div>
+                      <p className="text-right font-bold text-gray-600">
+                        {product.name}
+                      </p>
+                      <p className="text-gray-700">
+                        giá: {Number(product.cost).toLocaleString()} đ
+                      </p>
+                      <p className="font-mono font-bold text-gray-600">
+                        quantity: {item.quantity}
+                      </p>
                     </div>
-                  );
-                }
-              })}
-          </div>
-          <p className="my-7 mr-5 text-xl font-bold text-gray-500">
-            Total Price: {totaPrice.toLocaleString()} đ
-          </p>
-          <button
-            className="w-40 rounded bg-orange-500 px-4 py-2 font-bold text-white transition duration-200 hover:bg-orange-700 hover:shadow-md"
-            onClick={() => navigate("/checkout")}
-          >
-            Checkout
-          </button>
+                    <button
+                      className="ml-10 mr-4"
+                      onClick={() => {
+                        deleteIndividual(product._id);
+                        setTotalPrice(totaPrice - product.cost * item.quantity);
+                      }}
+                    >
+                      <IoIosCloseCircle className="text-4xl text-gray-200 transition duration-100 hover:text-red-500" />
+                    </button>
+                  </div>
+                );
+              }
+            })}
         </div>
-      </>
-    );
-  }
+        <p className="my-7 mr-5 text-xl font-bold text-gray-500">
+          Total Price: {totaPrice.toLocaleString()} đ
+        </p>
+        <button
+          className="w-40 rounded bg-orange-500 px-4 py-2 font-bold text-white transition duration-200 hover:bg-orange-700 hover:shadow-md"
+          onClick={() => navigate("/checkout")}
+        >
+          Checkout
+        </button>
+      </div>
+    </>
+  );
 }
