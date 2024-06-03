a3e5c76b2dc0603b652b2578fbd9bdf38a1de88e
QuangAnhTo <quangato072@gmail.com>
2024-06-03 09:43:46 +0700

    Refactor Navbar component for better code readability

--- src/components/navigations/Navbar.jsx
+++ src/components/navigations/Navbar.jsx
@@ -3,20 +3,36 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { IoLogInOutline } from "react-icons/io5";
 import { IoHeartOutline } from "react-icons/io5";
 import { IoCartOutline } from "react-icons/io5";
+import { MdOutlineShoppingCartCheckout } from "react-icons/md";
 import { useSelector } from "react-redux";
 import "./styles.css";
 
 export default function Navbar({ className }) {
+  /**------------------------------------------------------------------------------------------------
+   *                                         State declaration
+   *------------------------------------------------------------------------------------------------**/
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const navigate = useNavigate();
+
+  // search bar
   const [inputValue, setInputValue] = React.useState("");
   const [searchResult, setSearchResult] = React.useState([]);
-  const products = useSelector((state) => state.products.products);
-  const navigate = useNavigate();
-  // console.log(products);
 
+  // get products from store
+  const products = useSelector((state) => state.products.products);
+
+  //!  console.log(products); // check if products is fetched, uncomment to check
+
+  /**------------------------------------------------------------------------------------------------
+   *                                         Function definition
+   *------------------------------------------------------------------------------------------------**/
+
+  // function to handle search bar
   function handleOnChange(e) {
-    // check if products is fully fetched the search
     const searchInput = e.target.value;
     setInputValue(searchInput);
+
+    // check if products is fully fetched and the searchbar is not empty
     if (products.length !== 0 && inputValue !== "") {
       const result = products.filter((product) =>
         product.name.toLowerCase().includes(searchInput.toLowerCase()),
@@ -27,19 +43,20 @@ export default function Navbar({ className }) {
       console.log(searchResult);
     }
   }
-
   function renderSearchResult() {
+    // limit the number of results to be displayed
     const displayLimit = searchResult.length > 5 ? 5 : searchResult.length;
-    // const displayBox = "hidden"
+
+    // if the search bar is empty then the result box is replaced by a hidden div
     if (inputValue === "" || searchResult.length === 0) {
-      return <div className="hidden"> as </div>;
+      return <div className="hidden"></div>;
     }
     return (
       <div className="absolute flex w-96 translate-y-16 flex-col gap-2 divide-y-2 divide-gray-100 rounded-md border-2 border-orange-300 bg-white p-3 shadow-md transition-transform duration-1000">
         {searchResult.slice(0, displayLimit).map((product) => (
           <div
             key={product._id}
-            className="flex flex-row justify-between items-center gap-2 px-2 pb-3 pt-4"
+            className="flex flex-row items-center justify-between gap-2 px-2 pb-3 pt-4"
             onClick={() => {
               navigate(`/converseall/product/${product._id}`);
               setInputValue("");
@@ -50,18 +67,22 @@ export default function Navbar({ className }) {
               src={product.images.img_2}
               alt="product"
             />
-            <div className="flex flex-col w-3/5 items-end justify-between">
-              <p className="font-light text-right">{product.name}</p>
-              <p className="text-gray-400 font-medium mt-1">$ {Number(product.cost).toLocaleString()}</p>
+            <div className="flex w-3/5 flex-col items-end justify-between">
+              <p className="text-right font-light">{product.name}</p>
+              <p className="mt-1 font-medium text-gray-400">
+                {Number(product.cost).toLocaleString()}
+              </p>
             </div>
           </div>
         ))}
       </div>
     );
   }
+  /*------------------------------------------------------------------------------------------------*/
 
   return (
     <nav className={className}>
+      {/* logo and navigation */}
       <div className="w-100 flex flex-row gap-10 font-sans font-bold">
         <NavLink to="/">
           <img
@@ -74,6 +95,7 @@ export default function Navbar({ className }) {
         <NavLink to="/about">About</NavLink>
       </div>
 
+      {/* right side of the navbar */}
       <div className="w-100 flex flex-row gap-10 font-sans font-bold">
         {/* search bar */}
         <div className="w-100 flex flex-col items-center gap-10">
@@ -86,10 +108,10 @@ export default function Navbar({ className }) {
               handleOnChange(e);
             }}
           />
-
-          {/* list of results */}
           {searchResult.length !== 0 && renderSearchResult()}
         </div>
+
+        {/* login, favourite, cart */}
         <NavLink className="text-2xl" to="/login">
           <IoLogInOutline />
         </NavLink>
@@ -97,8 +119,14 @@ export default function Navbar({ className }) {
           <IoHeartOutline />
         </NavLink>
         <NavLink className="text-2xl" to="/cart">
-          <IoCartOutline />
+          {/* if products is existed in cart then change the icon */}
+          {cart.length !== 0 || !cart ? (
+            <MdOutlineShoppingCartCheckout />
+          ) : (
+            <IoCartOutline />
+          )}
         </NavLink>
+        
       </div>
     </nav>
   );
