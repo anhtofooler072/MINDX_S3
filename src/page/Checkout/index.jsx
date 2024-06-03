1caa2160050a4bd56b84105f9f71242fd2383682
QuangAnhTo <quangato072@gmail.com>
2024-06-03 15:07:11 +0700

    Feat: navigate to login page automatically when user isn't logged in

--- src/page/Checkout/index.jsx
+++ src/page/Checkout/index.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export default function Checkout() {
+  const loginState = useSelector((state) => state.isLoggedIn.isLoggedIn);
+  const navigate = useNavigate();
+  React.useEffect(() => {   
+    if (!loginState) {
+      navigate("/login");
+    }
+  }, [loginState, navigate]);
   return (
-    <div>index</div>
-  )
+    <div className="pt-48">
+      <p>Checkout</p>
+    </div>
+  );
 }
