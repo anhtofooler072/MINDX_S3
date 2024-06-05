bcd65bd6791154e2d1adb1fdb67c8e731c240de2
QuangAnhTo <quangato072@gmail.com>
2024-06-05 17:21:21 +0700

    Refactor CredentialForm component to improve code organization and readability

--- src/components/Registeration/CredentialFrom/index.jsx
+++ src/components/Registeration/CredentialFrom/index.jsx
@@ -1,39 +1,49 @@
 import React from "react";
 import SignUpForm from "../SignUp";
-import { useDispatch } from "react-redux";
-import { register } from "../../../store/isLoggedInSlice";
 import SignInForm from "../SignIn";
+import ReactLoading from "react-loading";
+import { useSelector } from "react-redux";
+import Popsup from "../../Popsup";
 
 export default function CredentialForm() {
   const [signUpState, setSignUpState] = React.useState(false);
-  const dispatch = useDispatch();
-
-  /*------------------------------------------------------------------------------------------------*/
+  const signUpPending = useSelector((state) => state.isLoggedIn.signUpState);
 
   if (signUpState === false) {
     return (
-      <div>
-        <SignInForm />
-        <button
-          type="button"
-          onClick={() => setSignUpState((prev) => !prev)}
-          className="mt-3 w-full rounded bg-orange-500 p-2 text-white hover:bg-orange-700"
-        >
-          Sign Up
-        </button>
+      <div className="flex h-screen w-full justify-center bg-gray-100 pt-56">
+        <div className="h-fit w-96 rounded-lg border-2 border-gray-200 bg-white p-6 shadow-lg">
+          <SignInForm />
+          <button
+            type="button"
+            onClick={() => setSignUpState((prev) => !prev)}
+            className="mt-3 w-full rounded bg-orange-500 p-2 text-white hover:bg-orange-700"
+          >
+            Sign Up
+          </button>
+        </div>
       </div>
     );
   } else {
     return (
-      <div>
-        <SignUpForm />
-        <button
-          type="button"
-          onClick={() => setSignUpState((prev) => !prev)}
-          className="mt-3 w-full rounded bg-orange-500 p-2 text-white hover:bg-orange-700"
-        >
-          Sign In
-        </button>
+      <div className="w-screen bg-gray-100">
+        {signUpPending && (
+          <Popsup>
+            <ReactLoading type={"spin"} color={"#fc531b"} className="mx-auto" />
+          </Popsup>
+        )}
+        <div className="flex h-screen w-full justify-center pt-40">
+          <div className="h-fit w-96 rounded-lg border-2 border-gray-200 bg-white p-6 shadow-lg">
+            <SignUpForm />
+            <button
+              type="button"
+              onClick={() => setSignUpState((prev) => !prev)}
+              className="mt-3 w-full rounded bg-orange-500 p-2 text-white hover:bg-orange-700"
+            >
+              Sign In
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
