198714211323333458a149b4dcdd9e5b2b620933
QuangAnhTo <quangato072@gmail.com>
2024-06-06 16:35:21 +0700

    feat: Add logout functionality to UserPage component

--- src/page/UserPage/index.jsx
+++ src/page/UserPage/index.jsx
@@ -1,14 +1,50 @@
 import React from "react";
+import { Logout as RdLgout} from "../../store/isLoggedInSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function UserPage() {
+  const loginState = useSelector((state) => state.isLoggedIn.isLoggedIn);
+  const username = useSelector((state) => state.isLoggedIn.username);
+  const email = useSelector((state) => state.isLoggedIn.email);
+  const phone_number = useSelector((state) => state.isLoggedIn.phone_number);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const logOut = () => {
+    dispatch(RdLgout());
+    navigate("/login");
+  };
+
+  useEffect(() => {
+    if (loginState === false) {
+      navigate("/login");
+    } else if (loginState === null) {
+      navigate("/login");
+    } else {
+      console.log("Username: ", username);
+      console.log("Email: ", email);
+      console.log("Phone number: ", phone_number);
+    }
+  }, [loginState, navigate]);
+
   return (
     <div className="pt-56">
-      <p>
-        login Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
-        nesciunt aperiam corporis odit, perferendis deleniti error tempora,
-        minima rem et qui hic explicabo commodi sequi. Vel ratione autem
-        assumenda iusto!
-      </p>
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-2xl font-bold">User Page</h1>
+        <div className="flex flex-col items-center gap-2">
+          <p>Username: {username}</p>
+          <p>Email: {email}</p>
+          <p>Phone number: {phone_number}</p>
+        </div>
+        <button
+          onClick={logOut}
+          className="mt-4 rounded bg-red-500 p-2 text-white"
+        >
+          log out
+        </button>
+      </div>
     </div>
   );
 }
