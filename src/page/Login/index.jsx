1bba6ce630d5538dee9942f31559ab2763f1069d
QuangAnhTo <quangato072@gmail.com>
2024-06-06 16:43:35 +0700

    feat: if the login state is true, then navigate to the user page

--- src/page/Login/index.jsx
+++ src/page/Login/index.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import CredentialForm from "../../components/Registeration/CredentialFrom";
 
 export default function Login() {
   const loginState = useSelector((state) => state.isLoggedIn.isLoggedIn);
-
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (loginState === true) {
+      navigate("/login/user");
+    }
+  }, [loginState, navigate]);
+
   if (loginState == false) {
     return (
       <div className="flex w-full flex-col items-center justify-center gap-4">
         <CredentialForm />
       </div>
     );
-  } else {
-    navigate("/login/user");
   }
-
 }
