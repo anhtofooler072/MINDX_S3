93bb77d9f8996f2c55b8aa4941c4dc47982d4d4e
QuangAnhTo <quangato072@gmail.com>
2024-06-06 16:40:58 +0700

    feat: check if there are token in side the local storage then getUserInfo

--- src/components/navigations/Layout.jsx
+++ src/components/navigations/Layout.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import Navbar from "./Navbar";
 import { Outlet, useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { getUserInfo } from "../../store/isLoggedInSlice";
 
 export default function Layout() {
     const location = useLocation();
     const navbarClass = "navbar"
     console.log(navbarClass)
+
+    const accessToken = localStorage.getItem("accessToken");
+    const dispatch = useDispatch();
+    const token = localStorage.getItem("accessToken");
+    React.useEffect(() => {
+        if (token) {
+            dispatch(getUserInfo(token));
+        }
+    }, [accessToken, dispatch]);
+
+
     return (
         <>
             <div>
